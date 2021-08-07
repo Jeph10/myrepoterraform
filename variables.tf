c08794dbb2aa27ecb1272004f0a344a60c4f9ac3
Jeph10 <73001294+Jeph10@users.noreply.github.com>
2021-08-07 01:48:37 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "launch-wizard-5"
+  default     = "launch-wizard-7"
 }
 
 variable "tag_name" {
